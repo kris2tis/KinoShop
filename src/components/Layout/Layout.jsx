70da7c01d3148dd0acc86d5f7c0b41d02893a617
mahdi <m.rezaei.coder@gmail.com>
2025-06-03 23:51:33 +0330

    Fix Path

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 import Header from "../common/header/Header";
-import Footer from "../common/Footer/Footer";
+import Footer from "../common/footer/Footer";
 
 export default function Layout() {
     return (
